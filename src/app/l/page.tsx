1d17b43acd81a6cb05fad2cdb4271644956c352f
donfsucha <cha1188@gmail.com>
2026-06-21 00:44:15 +0900

    Update Living Life video for 2026-06-21

--- src/app/l/page.tsx
+++ src/app/l/page.tsx
@@ -1,4 +1,4 @@
-const youtubeVideoId = "Cr389bUzDaM";
+const youtubeVideoId = "KuE94WBUON8";
 const playStoreUrl =
   "https://play.google.com/store/apps/details?id=com.cnanfc.xcanplayer&pcampaignid=web_share";
 
